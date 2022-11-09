4c0d44179fdebcc09169bf417921216cfa735c55
Camilledar <112490932+Camilledar@users.noreply.github.com>
2022-11-09 16:18:23 +0100

    Update snake_formatté.py

--- snake_formatté.py
+++ snake_formatté.py
@@ -1,12 +1,63 @@
-import random
-import sys
-import pygame
-import numpy as np
+# import random
+# import sys
+# import pygame
+# import numpy as np
 
-pygame.init()
-taille = 1000
-screen = pygame.display.set_mode([taille, taille])
-clock = pygame.time.Clock()
+# pygame.init()
+# taille = 800
+# screen = pygame.display.set_mode([taille, taille])
+# clock = pygame.time.Clock()
+# snake = [
+#     [700, 20],
+#     [720, 20],
+#     [740, 20],
+# ]
+
+# width = 20
+# height = 20
+
+# fps = 10
+
+# # palette de couleurs
+# red = 50
+# green = 0
+# blue = 150
+# color = [red, green, blue]
+# colorS = [200, 0, 0]
+# blanc = [255, 255, 255]
+# noir = [0, 0, 0]
+# colorFruit = [255, 255, 255]
+# COLORS = {"case": color, "background": noir, "snake": colorS, "fruit": colorFruit}
+
+# # générer fruit
+# xF, yF = random.randint(0, taille / 20 - 1), random.randint(0, taille / 20 - 1)
+# pygame.draw.rect(screen, COLORS["fruit"], [xF * 20, yF * 20, width, height])
+
+# # damier
+# x, y = 0, 0
+# for i in range(taille // 20):
+#     for j in range(taille // 20):
+#         rect = [x, y, width, height]
+#         pygame.draw.rect(screen, COLORS["case"], rect)
+#         y += 40
+#     if (i + 1) % 2 == 0:
+#         y = 0
+#     else:
+#         y = 20
+#     x += 20
+
+# # Direction
+# UP = [0, -1]
+# DOWN = [0, 1]
+# LEFT = [-1, 0]
+# RIGHT = [1, 0]
+# direction = LEFT
+
+# # sortie
+# def exit():
+#     pygame.quit()
+#     sys.exit()
+#     return
 
 while True:
     for event in pygame.event.get():
@@ -18,78 +69,58 @@ while True:
                 pygame.quit()
             elif event.key == pygame.K_UP:
                 print("↑")
-                [x, y] = snake[0]
-                rect = [x, y, width, height]
-                pygame.draw.rect(screen, noir, rect)
-                for i in range(2):
-                    snake[2][i] += direction[i]
-                print(direction)
-                serpent(snake)
+                direction = UP
             elif event.key == pygame.K_DOWN:
                 print("↓")
+                direction = DOWN
             elif event.key == pygame.K_LEFT:
                 print("←")
+                direction = LEFT
             elif event.key == pygame.K_RIGHT:
                 print("→")
+                direction = RIGHT
 
-    # damier
-    width = 20
-    height = 20
-    # red = 255
-    # green = 255
-    # blue = 255
-    # color = [red, green, blue]
-    # x,y= 0,0
-    # for i in range (taille//20):
-    # for j in range (taille//20):
-    # rect = [x, y, width, height]
-    # pygame.draw.rect(screen, color, rect)
-    # y+=40
-    # if (i+1)%2==0:
-    # y=0
-    # else:
-    # y=20
-    # x+=20
+    # effacer la queue
+    queue = snake[-1]
+    if ((queue[0] + queue[1]) / 20) % 2 == 0:
+        pygame.draw.rect(screen, COLORS["case"], [queue[0], queue[1], width, height])
+        pygame.display.update()
+    else:
+        pygame.draw.rect(
+            screen, COLORS["background"], [queue[0], queue[1], width, height]
+        )
+        pygame.display.update()
+    # modifier la liste snake
+    for i in range(1, len(snake)):
+        snake[len(snake) - i] = snake[len(snake) - i - 1]
+    snake[0] = [snake[0][0] + direction[0] * 20, snake[0][1] + direction[1] * 20]
+    # colorier le serpent
+    for i in range(len(snake)):
+        xS, yS = snake[i]
+        rectS = [xS, yS, width, height]
+        pygame.draw.rect(screen, COLORS["snake"], rectS)
+        pygame.display.update()
+    # regénérer le fruit et grandir
+    if snake[0] == [xF * 20, yF * 20]:
+        xF, yF = random.randint(0, taille / 20 - 1), random.randint(0, taille / 20 - 1)
+        COLORS["fruit"] = [
+            random.randint(0, 255),
+            random.randint(0, 255),
+            random.randint(0, 255),
+        ]
 
-    # serpent
-
-    snake = [
-        [10, 20],
-        [30, 20],
-        [50, 20],
-    ]
-
-    colorS = [255, 0, 0]
-
-    def serpent(snake):
-        for i in range(3):
-            [xS, yS] = snake[i]
-            rectS = [xS, yS, width, height]
-            pygame.draw.rect(screen, colorS, rectS)
-        return ()
-
-    serpent(snake)
-    noir = [0, 0, 0]
-
-    def colorier_noir(l):
-        noir = [0, 0, 0]
-        [x, y] = l
-        rect = [x, y, width, height]
-        pygame.draw.rect(screen, noir, rect)
-        return ()
-
-    # serpent qui bouge
-    direction = [20, 0]
-    # colorier_noir(snake[0])
-    # [x,y]=snake[0]
-    # rect=[x,y,width,height]
-    # pygame.draw.rect(screen, noir, rect)
-    # for i in range (2):
-    # snake[2][i]+=direction[i]
-    # print(snake)
-    # serpent(snake)
-    # elif event.key == pygame.K_DOWN:
-    # print('down')
-
+        pygame.draw.rect(screen, COLORS["fruit"], [xF * 20, yF * 20, width, height])
+        snake.append(snake[-1])
+        print(snake)
+    # test d'echec:
+    for i in range(1, len(snake)):
+        if snake[0] == snake[i]:
+            pygame.quit()
+            sys.exit()
+    if not (snake[0][0] in np.arange(0, taille, 20)) or not (
+        snake[0][1] in np.arange(0, taille, 20)
+    ):
+        pygame.quit()
+        sys.exit()
     pygame.display.update()
-    # clock.tick(1)
+    clock.tick(fps)
